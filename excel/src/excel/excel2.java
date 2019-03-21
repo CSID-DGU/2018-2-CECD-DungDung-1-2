e5294b35af6d4ba44c0c98fb248dc3d07118857c
DESKTOP-A7PGM1M\NA <loversugar@hanmail.net>
2019-03-22 00:41:12 +0900

    random

--- excel/src/excel/excel2.java
+++ excel/src/excel/excel2.java
@@ -21,14 +21,17 @@ public class excel2 {
 	
 	//**************words갯수, 출력갯수, 파일명3개바꾸고, 바꾸려는단어교체***************
 	public static void main(String[] args) {		
-		String words[] = new String[96];	//강의명,교수명 갯수
-		String sentences[] = new String[179];	//바꾸고자하는 문장 갯수
-		String save_sentences[] = new String[537];	//저장하고자하는 문장 갯수(x3)
+		String words[] = new String[88];	//강의명,교수명 갯수
+		String words1[] = new String[88];
+		String words2[] = new String[88];
+		String words3[] = new String[88];
+		String sentences[] = new String[1797];	//바꾸고자하는 문장 갯수
+		String save_sentences[] = new String[5391];	//저장하고자하는 문장 갯수(x3)
 
 		//단어 배열 만들기
 		try {
 			//파일 불러오기
-			FileInputStream word = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/강의명(96)_2.xlsx");
+			FileInputStream word = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/강의명(88).xlsx");
 			XSSFWorkbook wordbook = new XSSFWorkbook(word);
 			
 			//sheet수 취득
@@ -67,10 +70,135 @@ public class excel2 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		try {
+			//파일 불러오기
+			FileInputStream word1 = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/강의명1(88).xlsx");
+			XSSFWorkbook wordbook1 = new XSSFWorkbook(word1);
+			
+			//sheet수 취득
+			//int sheetCn = workbook.getNumberOfSheets();
+			int sheetCn=1;			
+			System.out.println("sheet수 : " + sheetCn);
+
+			//시트만큼 반복
+			for(int cn = 0; cn < sheetCn; cn++){
+				System.out.println("취득하는 sheet 이름 : " + wordbook1.getSheetName(cn));
+				//0번째 sheet 정보 취득
+				XSSFSheet sheet = wordbook1.getSheetAt(cn);
+				
+				//취득된 sheet에서 rows수 취득
+				int rows = sheet.getPhysicalNumberOfRows();
+				System.out.println(wordbook1.getSheetName(cn) + " sheet의 row수 : " + rows);
+
+				//취득된 row에서 취득대상 cell수 취득
+				int cells = sheet.getRow(cn).getPhysicalNumberOfCells();
+				System.out.println(wordbook1.getSheetName(cn) + " sheet의 row에 취득대상 cell수 : " + cells);
+
+				for (int r = 0; r < rows; r++) {
+					row = sheet.getRow(r); // row 가져오기
+					if (row != null) {
+						for (int c = 0; c < cells; c++) {
+							cell = row.getCell(c);
+							if (cell != null) {								
+								words1[r] = "" + cell.getStringCellValue();								
+							} else {
+								System.out.print("cell이 없습니다\t");
+							}
+						} 
+					}
+				} 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			//파일 불러오기
+			FileInputStream word2 = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/강의명2(88).xlsx");
+			XSSFWorkbook wordbook2 = new XSSFWorkbook(word2);
+			
+			//sheet수 취득
+			//int sheetCn = workbook.getNumberOfSheets();
+			int sheetCn=1;			
+			System.out.println("sheet수 : " + sheetCn);
+
+			//시트만큼 반복
+			for(int cn = 0; cn < sheetCn; cn++){
+				System.out.println("취득하는 sheet 이름 : " + wordbook2.getSheetName(cn));
+				//0번째 sheet 정보 취득
+				XSSFSheet sheet = wordbook2.getSheetAt(cn);
+				
+				//취득된 sheet에서 rows수 취득
+				int rows = sheet.getPhysicalNumberOfRows();
+				System.out.println(wordbook2.getSheetName(cn) + " sheet의 row수 : " + rows);
+
+				//취득된 row에서 취득대상 cell수 취득
+				int cells = sheet.getRow(cn).getPhysicalNumberOfCells();
+				System.out.println(wordbook2.getSheetName(cn) + " sheet의 row에 취득대상 cell수 : " + cells);
+
+				for (int r = 0; r < rows; r++) {
+					row = sheet.getRow(r); // row 가져오기
+					if (row != null) {
+						for (int c = 0; c < cells; c++) {
+							cell = row.getCell(c);
+							if (cell != null) {								
+								words2[r] = "" + cell.getStringCellValue();								
+							} else {
+								System.out.print("cell이 없습니다\t");
+							}
+						} 
+					}
+				} 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			//파일 불러오기
+			FileInputStream word3 = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/강의명3(88).xlsx");
+			XSSFWorkbook wordbook3 = new XSSFWorkbook(word3);
+			
+			//sheet수 취득
+			//int sheetCn = workbook.getNumberOfSheets();
+			int sheetCn=1;			
+			System.out.println("sheet수 : " + sheetCn);
+
+			//시트만큼 반복
+			for(int cn = 0; cn < sheetCn; cn++){
+				System.out.println("취득하는 sheet 이름 : " + wordbook3.getSheetName(cn));
+				//0번째 sheet 정보 취득
+				XSSFSheet sheet = wordbook3.getSheetAt(cn);
+				
+				//취득된 sheet에서 rows수 취득
+				int rows = sheet.getPhysicalNumberOfRows();
+				System.out.println(wordbook3.getSheetName(cn) + " sheet의 row수 : " + rows);
+
+				//취득된 row에서 취득대상 cell수 취득
+				int cells = sheet.getRow(cn).getPhysicalNumberOfCells();
+				System.out.println(wordbook3.getSheetName(cn) + " sheet의 row에 취득대상 cell수 : " + cells);
+
+				for (int r = 0; r < rows; r++) {
+					row = sheet.getRow(r); // row 가져오기
+					if (row != null) {
+						for (int c = 0; c < cells; c++) {
+							cell = row.getCell(c);
+							if (cell != null) {								
+								words3[r] = "" + cell.getStringCellValue();								
+							} else {
+								System.out.print("cell이 없습니다\t");
+							}
+						} 
+					}
+				} 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 		
 		//문장 배열 만들기 + 저장
 		try {
-			FileInputStream sentence = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/2019_01_06_10차(교수님질문).xlsx");
+			FileInputStream sentence = new FileInputStream("C:\\Users\\NA\\git\\Project\\Dataset\\데이터셋/2019_03_14_14차(교수님+강의질문)_RAN.xlsx");
 			XSSFWorkbook sentencebook = new XSSFWorkbook(sentence);
 			//sheet수 취득
 			//int sheetCn = workbook.getNumberOfSheets();
@@ -109,7 +237,10 @@ public class excel2 {
 								for(int i=0;i<3;i++)
 								{
 									int random_num = (int)(Math.random()*words.length);
-									save_sentences[count] = save_sentences[count].replace("[[교수님_성함]]", words[random_num]);
+									save_sentences[count] = save_sentences[count].replace("[[강의명]]", words[random_num]);
+									save_sentences[count] = save_sentences[count].replace("[[1]]", words1[random_num]);
+									save_sentences[count] = save_sentences[count].replace("[[2]]", words2[random_num]);
+									save_sentences[count] = save_sentences[count].replace("[[3]]", words3[random_num]);
 									count++;
 								}														
 							} else {								
@@ -150,7 +281,7 @@ public class excel2 {
 		// 출력 파일 위치및 파일명 설정
 		FileOutputStream outFile;
 		try {
-			outFile = new FileOutputStream("2019_01_06_10차(교수님질문)_RANN.xlsx");
+			outFile = new FileOutputStream("2019_03_14_14차(교수님+강의질문)_RAN.xlsx");
 			writebook.write(outFile);
 			outFile.close();
 			System.out.println("파일생성 완료");
